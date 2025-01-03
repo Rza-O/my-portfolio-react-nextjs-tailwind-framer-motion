4d8986f94040ef461f43e1cbd4d36a212738eda5
appoclayptor <shahreza1414@gmail.com>
2025-01-04 03:27:49 +0600

    added hamburger toogle menu for small devices

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,8 +1,18 @@
 import { assets } from '@/assets/assets';
 import Image from 'next/image';
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Navbar = () => {
+   const sideMenuRef = useRef();
+   const openMenu = () => {
+      sideMenuRef.current.style.transform = 'translateX(-16rem)'
+   }
+   const closeMenu = () => {
+      sideMenuRef.current.style.transform = 'translateX(16rem)'
+   }
+
+
+
    return (
       <div>
          <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -31,8 +41,26 @@ const Navbar = () => {
                   <Image src={assets.moon_icon} alt='' className='w-6'></Image>
                </button>
 
-               <a href="#contact" className='hidden md:flex gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 items-center font-ovo'>Contact <Image src={assets.arrow_icon} className='w-3' alt=''/></a>
+               <a href="#contact" className='hidden md:flex gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 items-center font-ovo'>Contact <Image src={assets.arrow_icon} className='w-3' alt='' /></a>
+
+               <button className='block md:hidden ml-3' onClick={openMenu}>
+                  <Image src={assets.menu_black} alt='' className='w-6'></Image>
+               </button>
             </div>
+
+            {/* Mobile menu */}
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+
+               <div className='absolute right-6 top-6' onClick={closeMenu}>
+                  <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'></Image>
+               </div>
+
+               <li><a className='font-ovo' onClick={closeMenu} href="#top">Home</a></li>
+               <li><a className='font-ovo' onClick={closeMenu} href="#about">About</a></li>
+               <li><a className='font-ovo' onClick={closeMenu} href="#skills">Skills</a></li>
+               <li><a className='font-ovo' onClick={closeMenu} href="#work">My Works</a></li>
+               <li><a className='font-ovo' onClick={closeMenu} href="#contact">Contact me</a></li>
+            </ul>
          </nav>
       </div>
    );
