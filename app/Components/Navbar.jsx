3ec359d85d45d81a2e422e5966b7bfc1c4c6009b
appoclayptor <shahreza1414@gmail.com>
2025-01-04 05:05:51 +0600

    made some responsive changes

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,8 +1,9 @@
 import { assets } from '@/assets/assets';
 import Image from 'next/image';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Navbar = () => {
+   const [isScroll, setIsScroll] = useState(false);
    const sideMenuRef = useRef();
    const openMenu = () => {
       sideMenuRef.current.style.transform = 'translateX(-16rem)'
@@ -11,14 +12,24 @@ const Navbar = () => {
       sideMenuRef.current.style.transform = 'translateX(16rem)'
    }
 
-
+   useEffect(() => {
+      window.addEventListener('scroll', () => {
+         if (scrollY > 50) {
+            setIsScroll(true);
+         }
+         else {
+            setIsScroll(false);
+         }
+      }
+      )
+   }, []);
 
    return (
       <div>
          <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
             <Image src={assets.header_bg_color} alt='' className='w-full'></Image>
          </div>
-         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between'>
+         <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
             {/* <a href="">
                <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt=''/>
             </a> */}
@@ -27,7 +38,7 @@ const Navbar = () => {
                <h2 className='text-4xl font-semibold'>Reza<span className='text-red-600 text-4xl font-bold'>.</span></h2>
             </a>
 
-            <ul className='hidden md:flex items-center gap-6 lg:gap-8 px-12 py-3 bg-white shadow-sm bg-opacity-50 rounded-full'>
+            <ul className={`hidden md:flex items-center gap-6 lg:gap-8 px-12 py-3 rounded-full ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"}`}>
                <li><a className='font-ovo' href="#top">Home</a></li>
                <li><a className='font-ovo' href="#about">About</a></li>
                <li><a className='font-ovo' href="#skills">Skills</a></li>
