8224773f7cdb7ca88ca4808b063ffdeb53285499
appoclayptor <shahreza1414@gmail.com>
2025-01-04 20:26:33 +0600

    added skills section

--- app/Components/MySkills.jsx
+++ app/Components/MySkills.jsx
@@ -1,32 +1,29 @@
-import { BorderBeam } from '@/components/ui/border-beam';
-import { infoList } from '@/assets/assets';
-import React from 'react';
+import { assets, serviceData } from '@/assets/assets';
 import Image from 'next/image';
-import Meteors from '@/components/ui/meteors';
+import React from 'react';
 
 const MySkills = () => {
    return (
       <div id='skills' className='w-full px-[12%] py-10 scroll-mt-20'>
          <h4 className='text-center mb-2 text-lg font-ovo'>What I Offer</h4>
-         <h2 className='text-center text-5xl font-ovo mb-8'>My Skills</h2>
+         <h2 className='text-center text-5xl font-ovo'>My Skills</h2>
+         <p className='text-center max-w-2xl mx-auto mt-6 mb-12 font-ovo'>I am a frontend developer from Dhaka, Bangladesh. My expertise lies in fostering collaboration, optimizing resources, and embracing new technologies to solve real-world challenges.</p>
 
-         <div className="relative flex min-h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-            {/* <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-               Border Beam
-            </span> */}
-            <Meteors number={50} />
-            <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
-               {infoList.map(({ icon, iconDark, title, description }, idx) => (
-                  <li key={idx} className='border-[0.5px] border-gray-400 rounded-xl px-8 py-3 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black flex flex-col items-center '>
-                     <Image src={icon} alt={title} className='w-12 mt-3'></Image>
-                     <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
-                     {/* <p className='text-gray-600 text-sm'>{description}</p> */}
-                  </li>
-               )
-               )}
-            </ul>
-            <BorderBeam size={250} duration={12} delay={9} />
+         <div className='grid grid-cols-auto gap-6 my-10'>
+            {
+               serviceData.map(({ icon, title, description, link }, idx) => (
+                  <div key={idx} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500'>
+                     <Image src={icon} alt='' className='w-10'></Image>
+                     <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
+                     <p className='text-sm text-gray-600 leading-5'>{description}</p>
+                     <a href={link} className='flex items-center gap-2 text-sm mt-5'>
+                        Read More <Image src={assets.right_arrow} className='w-4' alt=''></Image>
+                     </a>
+                  </div>
+               ))
+            }
          </div>
+
       </div>
    );
 };
