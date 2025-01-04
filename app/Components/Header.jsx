2e7d9c3e2354dc92860e3ffd409f9f5da15ff74a
appoclayptor <shahreza1414@gmail.com>
2025-01-04 23:01:38 +0600

    added motion react animation in the header section

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,24 +1,53 @@
 import { assets } from '@/assets/assets';
 import Image from 'next/image';
 import React from 'react';
+import { motion } from "motion/react"
 
 const Header = () => {
    return (
       <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
-         <div>
+         <motion.div
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+            transition={{ duration: 0.8, type: 'spring', stiffness: 100 }}
+         >
             <Image src={assets.profile_img} alt='' className='rounded-full w-28 lg:w-32 mt-9'></Image>
-         </div>
-         <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-ovo'>Hi! I'm Shah Reza  <Image src={assets.hand_icon} alt='' className='w-6'></Image></h3>
-         <h1 className='text-3xl sm:text-6xl lg:[66px] font-ovo'>frontend web developer based in Bangladesh</h1>
-         <p className='max-w-2xl mx-auto font-ovo'>
-            I am a frontend developer from Dhaka, Bangladesh. My expertise lies in fostering collaboration, optimizing resources, and embracing new technologies to solve real-world challenges.
-         </p>
-         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <a className='px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2 dark:bg-transparent' href="#contact">
-               Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'></Image></a>
+         </motion.div>
 
-            <a href="/ShahReza.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
-               My Resume<Image src={assets.download_icon} alt='' className='w-4'></Image></a>
+         <motion.h3
+            initial={{ y: -20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 0.3 }}
+            className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-ovo'>Hi! I'm Shah Reza  <Image src={assets.hand_icon} alt='' className='w-6'></Image></motion.h3>
+
+         <motion.h1
+            initial={{ y: -30, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.5 }}
+            className='text-3xl sm:text-6xl lg:[66px] font-ovo'>frontend web developer based in Bangladesh</motion.h1>
+
+         <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.6, delay: 0.7 }}
+            className='max-w-2xl mx-auto font-ovo'>
+            I am a frontend developer from Dhaka, Bangladesh. My expertise lies in fostering collaboration, optimizing resources, and embracing new technologies to solve real-world challenges.
+         </motion.p>
+
+         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+            <motion.a
+               initial={{ y: 30, opacity: 0 }}
+               whileInView={{ y: 0, opacity: 1 }}
+               transition={{ duration: 0.6, delay: 1 }}
+               className='px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2 dark:bg-transparent' href="#contact">
+               Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'></Image></motion.a>
+
+            <motion.a
+               initial={{ y: 30, opacity: 0 }}
+               whileInView={{ y: 0, opacity: 1 }}
+               transition={{ duration: 0.6, delay: 1.2 }}
+               href="/ShahReza.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
+               My Resume<Image src={assets.download_icon} alt='' className='w-4'></Image></motion.a>
          </div>
       </div>
    );
