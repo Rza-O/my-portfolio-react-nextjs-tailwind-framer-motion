d04e5e7b6959da8c39bd9e0341166feab78741e1
appoclayptor <shahreza1414@gmail.com>
2025-01-05 08:33:52 +0600

    added a favicon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,132 +7,132 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-    	extend: {
-    		colors: {
-    			lightHover: '#fcf4ff',
-    			darkHover: '#2a004a',
-    			darkTheme: '#11001f',
-    			background: 'hsl(var(--background))',
-    			foreground: 'hsl(var(--foreground))',
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			popover: {
-    				DEFAULT: 'hsl(var(--popover))',
-    				foreground: 'hsl(var(--popover-foreground))'
-    			},
-    			primary: {
-    				DEFAULT: 'hsl(var(--primary))',
-    				foreground: 'hsl(var(--primary-foreground))'
-    			},
-    			secondary: {
-    				DEFAULT: 'hsl(var(--secondary))',
-    				foreground: 'hsl(var(--secondary-foreground))'
-    			},
-    			muted: {
-    				DEFAULT: 'hsl(var(--muted))',
-    				foreground: 'hsl(var(--muted-foreground))'
-    			},
-    			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
-    			},
-    			destructive: {
-    				DEFAULT: 'hsl(var(--destructive))',
-    				foreground: 'hsl(var(--destructive-foreground))'
-    			},
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
-    			chart: {
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
-    			},
-    			'color-1': 'hsl(var(--color-1))',
-    			'color-2': 'hsl(var(--color-2))',
-    			'color-3': 'hsl(var(--color-3))',
-    			'color-4': 'hsl(var(--color-4))',
-    			'color-5': 'hsl(var(--color-5))'
-    		},
-    		fontFamily: {
-    			outfit: [
-    				'Outfit',
-    				'sans-serif'
-    			],
-    			ovo: [
-    				'Ovo',
-    				'serif'
-    			]
-    		},
-    		boxShadow: {
-    			black: '4px 4px 0 #000',
-    			white: '4px 4px 0 #fff'
-    		},
-    		gridTemplateColumns: {
-    			auto: 'repeat(auto-fit, minmax(200px, 1fr))'
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		animation: {
-    			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
-    			meteor: 'meteor 5s linear infinite',
-    			grid: 'grid 15s linear infinite',
-    			ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite',
-    			rainbow: 'rainbow var(--speed, 2s) infinite linear'
-    		},
-    		keyframes: {
-    			'border-beam': {
-    				'100%': {
-    					'offset-distance': '100%'
-    				}
-    			},
-    			meteor: {
-    				'0%': {
-    					transform: 'rotate(215deg) translateX(0)',
-    					opacity: '1'
-    				},
-    				'70%': {
-    					opacity: '1'
-    				},
-    				'100%': {
-    					transform: 'rotate(215deg) translateX(-500px)',
-    					opacity: '0'
-    				}
-    			},
-    			grid: {
-    				'0%': {
-    					transform: 'translateY(-50%)'
-    				},
-    				'100%': {
-    					transform: 'translateY(0)'
-    				}
-    			},
-    			ripple: {
-    				'0%, 100%': {
-    					transform: 'translate(-50%, -50%) scale(1)'
-    				},
-    				'50%': {
-    					transform: 'translate(-50%, -50%) scale(0.9)'
-    				}
-    			},
-    			rainbow: {
-    				'0%': {
-    					'background-position': '0%'
-    				},
-    				'100%': {
-    					'background-position': '200%'
-    				}
-    			}
-    		}
-    	}
-    },
+		extend: {
+			colors: {
+				lightHover: '#fcf4ff',
+				darkHover: '#2a004a',
+				darkTheme: '#11001f',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				},
+				'color-1': 'hsl(var(--color-1))',
+				'color-2': 'hsl(var(--color-2))',
+				'color-3': 'hsl(var(--color-3))',
+				'color-4': 'hsl(var(--color-4))',
+				'color-5': 'hsl(var(--color-5))'
+			},
+			fontFamily: {
+				outfit: [
+					'Outfit',
+					'sans-serif'
+				],
+				ovo: [
+					'Ovo',
+					'serif'
+				]
+			},
+			boxShadow: {
+				black: '4px 4px 0 #000',
+				white: '4px 4px 0 #fff'
+			},
+			gridTemplateColumns: {
+				auto: 'repeat(auto-fit, minmax(200px, 1fr))'
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			animation: {
+				'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
+				meteor: 'meteor 5s linear infinite',
+				grid: 'grid 15s linear infinite',
+				ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite',
+				rainbow: 'rainbow var(--speed, 2s) infinite linear'
+			},
+			keyframes: {
+				'border-beam': {
+					'100%': {
+						'offset-distance': '100%'
+					}
+				},
+				meteor: {
+					'0%': {
+						transform: 'rotate(215deg) translateX(0)',
+						opacity: '1'
+					},
+					'70%': {
+						opacity: '1'
+					},
+					'100%': {
+						transform: 'rotate(215deg) translateX(-500px)',
+						opacity: '0'
+					}
+				},
+				grid: {
+					'0%': {
+						transform: 'translateY(-50%)'
+					},
+					'100%': {
+						transform: 'translateY(0)'
+					}
+				},
+				ripple: {
+					'0%, 100%': {
+						transform: 'translate(-50%, -50%) scale(1)'
+					},
+					'50%': {
+						transform: 'translate(-50%, -50%) scale(0.9)'
+					}
+				},
+				rainbow: {
+					'0%': {
+						'background-position': '0%'
+					},
+					'100%': {
+						'background-position': '200%'
+					}
+				}
+			}
+		}
+	},
 	darkMode: 'selector',
 	plugins: [require("tailwindcss-animate")],
 }
